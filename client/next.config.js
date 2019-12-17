33fc812860cab3bb07c588a6dbbc2a4983355950
afedorchenko <afedorchenko@rendmate.com>
2019-12-17 12:43:43 +0200

    small change

--- client/next.config.js
+++ client/next.config.js
@@ -1,6 +1,2 @@
 const withSass = require('@zeit/next-sass')
-module.exports = withSass({
-  sassLoaderOptions: {
-    includePaths: ["absolute/path/a", "absolute/path/b"]
-  }
-})
+module.exports = withSass()
