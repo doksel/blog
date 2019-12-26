40e72c23dafa4aa653582ebc70c66b4ee016db93
afedorchenko <afedorchenko@rendmate.com>
2019-12-26 10:44:43 +0200

    fix graphql

--- client/src/pages/FirstPage.js
+++ client/src/pages/FirstPage.js
@@ -1,19 +1,35 @@
 import React, {Component} from "react";
 import {connect} from "react-redux";
+import {graphql} from "react-apollo";
+import {compose} from "recompose";
+import  gql  from "graphql-tag";
 
 class FirstPage extends Component {
   render(){
-    const {login}=this.props;
+    const {
+      login, 
+      data: {loading, hi}
+    }=this.props;
     console.log('PROPS',this.props);
     
+    if(loading){
+      return <div>Loading......</div>
+    }
+
     return (
       <div>
         <div>first page</div>
-        <div>Hello </div>
+        <div>Hello - {hi}</div>
       </div>
     )
   };
 };
+
+const hiQuery = gql`
+  {
+    hi
+  }
+`
 
 const mapStateToProps = state => ({
   login: state.me.login
@@ -21,4 +37,7 @@ const mapStateToProps = state => ({
 
 const mapDispatchToProps = {};
 
-export default connect(mapStateToProps, mapDispatchToProps)(FirstPage);
+export default compose(
+  graphql(hiQuery),
+  connect(mapStateToProps, mapDispatchToProps)
+  )(FirstPage);
