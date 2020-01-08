18c4f3646965c436b384c0d21c294967f0371271
afedorchenko <afedorchenko@rendmate.com>
2020-01-08 11:31:55 +0200

    change in next

--- client/.next/static/runtime/webpack.js
+++ client/.next/static/runtime/webpack.js
@@ -113,7 +113,7 @@
 /******/
 /******/ 	var hotApplyOnUpdate = true;
 /******/ 	// eslint-disable-next-line no-unused-vars
-/******/ 	var hotCurrentHash = "e3bfad9f865a4b2f52b0";
+/******/ 	var hotCurrentHash = "4e92d09bb432e128fa9e";
 /******/ 	var hotRequestTimeout = 10000;
 /******/ 	var hotCurrentModuleData = {};
 /******/ 	var hotCurrentChildModule;
